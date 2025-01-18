d126ca67495d37c70f2dafe49cf33256eea13571
Jawwad926 <mdsabbirhosen926@gmail.com>
2025-01-18 16:20:55 +0600

    feat: add concept of deal with predefined images, interactive mode of containers

---
+++ Python Practice/myapp.py
@@ -0,0 +1,9 @@
+print("Program to sum two numbers: ")
+
+a = int(input('Enter the first number : '))
+b = int(input("Enter the second number : "))
+
+result = a+b
+
+print(f'Sum of two numbers are : {result}')
+ 
